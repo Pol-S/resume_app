ebe55c06105c77ed78b47150db3084cb1d81a5b9
Pol-S <plsbnjy@gmail.com>
2020-05-04 20:31:35 -0500

    Got John Smith fake data to display on Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>{{students}}</p>
   </div>
 </template>
 
@@ -14,26 +15,32 @@ export default {
     return {
       message: "Welcome to Arbys!",
       student: {},
+      students: [],
     };
   },
+  // created: function() {
+  // axios.get("/api/students" + this.$route.params.id).then(response => {
+  //   this.students = response.data;
+  // });
+
   created: function() {
-    // axios.get("/api/students" + this.$route.params.id).then(response => {
-    //   this.students = response.data;
-    // });
-    return { student.create( 
-      first_name: "John",
-      last_name: "Smith",
-      email: "smith@example.com",
-      password_digest: "password",
-      phone_number: "333 444-5555",
-      short_bio: "Was was a man from nantucket",
-      linkedin_url: "linkedin.com/jsmith",
-      twitter_handle: "smithers@twitter.com",
-      personal_website: "koala.com",
-      resume_url: "jsmithresumes.com",
-      github_url: "github.com/josmi",
-      photo: "https://thenypost.files.wordpress.com/2019/11/godzilla-34.jpg?quality=90&strip=all&w=618&h=410&crop=1")
-    };
+    this.students = [
+      {
+        first_name: "John",
+        last_name: "Smith",
+        email: "smith@example.com",
+        password_digest: "password",
+        phone_number: "(333) 444-5555",
+        short_bio: "Once was a man from nantucket",
+        linkedin_url: "linkedin.com/jsmith",
+        twitter_handle: "smithers@twitter.com",
+        personal_website: "koala.com",
+        resume_url: "jsmithresumes.com",
+        github_url: "github.com/josmi",
+        photo: "https://thenypost.files.wordpress.com/2019/11/godzilla-34.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+      },
+    ];
+    this.student = this.students[0];
   },
 
   methods: {},
